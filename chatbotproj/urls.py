3548f7dd85878946d29ceaa75d4355b749b54dd3
mr-robot369 <anubhavshukla870@gmail.com>
2025-06-17 11:29:48 +0530

    admin header change

--- chatbotproj/urls.py
+++ chatbotproj/urls.py
@@ -18,6 +18,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+admin.site.site_header = "CUH Admin Panel"
+admin.site.site_title = "CUH Site Admin"
+admin.site.index_title = "Welcome to Central University of Haryana Admin Portal"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('chatapp.urls')),
